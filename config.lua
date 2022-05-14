81d05bc9559ed86f1bf273bb4e4f0743437d71aa
captnbuckshot <brandop71@gmail.com>
2022-05-14 18:09:56 -0400

    Add whitelist

--- config.lua
+++ config.lua
@@ -2,6 +2,8 @@ Config = Config or {}
 
 Config.WhitelistedCreators = {
     "PUTCID",
+    '76561198093964736', -- Dibble Steam ID
+    '76561197970236152', -- Captn Buckshot Steam ID
 }
 
 Config.RaceSetupAllowed = true
